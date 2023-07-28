41b71b11de5ded64230d30fc248f725863b55e15
shahwaizse <mshahwaiz12@gmail.com>
2023-07-28 19:47:00 +0500

    refactored promise syntax to async/await

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const img = document.querySelector("img");
 const api_key = ""; //enter your GIPHY api key here.
-function fetchGif() {
-    img.src = "./loading.gif";
-    let searchContent = document.getElementById("searchBox").value;
-    fetch('https://api.giphy.com/v1/gifs/translate?api_key='+api_key+'&s='+searchContent, {mode: 'cors'})
-    .then((response) => {
-        return response.json();
-    })
-    .then((response) => {
-        img.src = response.data.images.original.url;
-    })
-    .catch((response) => {
+async function fetchGif() {
+    try{
+        img.src = "./loading.gif";
+        let searchContent = document.getElementById("searchBox").value;
+        const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key='+api_key+'&s='+searchContent, {mode: 'cors'});
+        const gifData = await response.json();
+        img.src = gifData.data.images.original.url;
+    }
+    catch(error){
         img.src = "./found.jpg";
-    });
+        console.log('error: ' + error);
+    }
 }
